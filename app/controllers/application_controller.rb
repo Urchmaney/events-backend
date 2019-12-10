04d15c40e699bd3521c928b466e4334b9f3596a4
Urchmaney <kingsobino@gmail.com>
2019-12-10 23:22:06 +0000

    Add authentication jwt token encode and decode functionality.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,40 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
+  def logged_in?
+    !!current_user
+  end
+  
+  def current_user
+    if auth_present?
+      if !auth
+        return nil
+      end
+      user = User.find(auth['user'])
+      if user
+        @current_user ||= user
+      end
+    end
+  end
+
+  def authenticate
+    render json: {error: "unauthorized"}, status: 401 unless logged_in?
+  end
+
+  private
+
+  def token
+    request.env["HTTP_AUTHORIZATION"].scan(/Bearer (.*)$/).flatten.last
+  end
+
+  def auth
+    Auth.decode(token)
+  end
+
+  def auth_present?
+    request.env["HTTP_AUTHORIZATION"].scan(/Bearer/).flatten.first
+  end
+
 end

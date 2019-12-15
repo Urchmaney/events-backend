66bdde26cd22af38f1805ae41e8b5d3990c8dfa1
Urchmaney <kingsobino@gmail.com>
2019-12-15 18:48:34 +0000

    Add event organizers model and migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_110354) do
+ActiveRecord::Schema.define(version: 2019_12_15_112945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2019_12_12_110354) do
   create_table "interests", force: :cascade do |t|
     t.integer "user_id"
     t.integer "event_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "organizers", force: :cascade do |t|
+    t.string "name"
+    t.string "role"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

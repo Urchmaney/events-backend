f562f2c006bb854974dad3f54c4145c72f17cd8b
Urchmaney <kingsobino@gmail.com>
2019-12-17 17:21:28 +0000

    Create endpoint to create events

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -8,6 +8,23 @@ class API::V1::EventsController < ApplicationController
   end
 
   def all
-    render json: Event.all , status: 200
+    render json: Event.all.where('start > ?', DateTime.now) , status: 200
+  end
+
+  def create
+    status = 200
+    result = 'Successfully Added Event.'
+    event = Event.create(events_params)
+    if !event.valid?
+      status = 400
+      result = { error: user.errors.full_messages }
+    end
+    render json: result , status: status
+  end
+
+  private
+
+  def events_params
+    params.require(:event).permit(:title, :description, :location, :start, :end, organizers_attributes: [:name, :role, :occupation])
   end
 end
